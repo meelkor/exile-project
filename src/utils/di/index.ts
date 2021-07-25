342ddf36dba8add31e4658d78823e2cf5007e50b
Jan Seda <sedajancz@gmail.com>
2021-07-25 14:08:24 +0200

    finish injectable

--- src/utils/di/index.ts
+++
@@ -1,5 +0,0 @@
-export * from '@exile/utils/di/injectable';
-export * from '@exile/utils/di/injectable-base';
-export * from '@exile/utils/di/injectable-value';
-export * from '@exile/utils/di/injectable-global';
-export * from '@exile/utils/di/injector';
--- src/utils/di/injectable-global-is.ts
+++
@@ -1,9 +0,0 @@
-import { InjectableGlobal } from '@exile/utils/di';
-import { Constructor } from '@exile/utils/types/class';
-
-export const $$GLOBAL = Symbol('Global Injectable');
-
-export function isGlobalInjectable(Class: Constructor<any>): Class is Constructor<InjectableGlobal> {
-    return $$GLOBAL in Class;
-}
-
