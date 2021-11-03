982205d6b4f4b2d837822968a2dc42df6d4f4f1c
Jan Seda <sedajancz@gmail.com>
2021-11-03 20:34:12 +0100

    fixup! inkarnate-clip: add support for y-scaling

--- packages/client/src/game/modules/overworld/resources/overworld-map-manifest.ts
+++ packages/client/src/game/modules/overworld/resources/overworld-map-manifest.ts
@@ -2,6 +2,7 @@ import { Pos } from '@exile/common/types/geometry';
 
 export interface OverworldMapManifest {
     chunks: ChunkManifest[];
+    yScale: number;
 }
 
 export interface ChunkManifest {
